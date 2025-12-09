ffc8359357280b9046820119142756a6a3dc4824
Pacifisity <97253406+Pacifisity@users.noreply.github.com>
2025-12-08 20:16:55 -0500

    .

--- Global.lua
+++ Global.lua
@@ -22,21 +22,38 @@ local matchedScriptURL = nil
 -- Search for a script whose filename matches the current game
 for _, item in ipairs(data) do
     if item.type == 'file' and item.name then
-        local fileName = item.name:match('^(%d+)$') -- extract pure numeric filenames only
+        local fileName = item.name:match('^(%d+)$') -- pure numeric filenames
 
         if fileName == gameId then
-            matchedScriptURL = item.download_url -- GitHub raw file URL
+            matchedScriptURL = item.download_url
             break
         end
     end
 end
 
+--=============================================================
+-- FALLBACK
+--=============================================================
 if not matchedScriptURL then
-    warn('No script found for this game.', game.GameId)
+
+    local defaultUI = "https://raw.githubusercontent.com/Pacifisity/LuaU/refs/heads/main/Sage.lua"
+
+    local ok, runUI = pcall(function()
+        return loadstring(game:HttpGet(defaultUI))
+    end)
+
+    if ok and runUI then
+        runUI()
+    else
+        warn("Failed to load default Sage UI.")
+    end
+
     return
 end
+--=============================================================
 
--- Load and execute the script
+
+-- Load and execute the matched script
 local ok, loaded = pcall(function()
     return loadstring(game:HttpGet(matchedScriptURL))
 end)
